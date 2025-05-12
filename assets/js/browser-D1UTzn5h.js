dd58e953f34572d98bebbab6ddd8c482bc5274e9
nicunursekatie <katielong2316@gmail.com>
2025-05-12 19:36:51 +0000

    Updates

--- assets/js/browser-D1UTzn5h.js
+++
@@ -1,2 +0,0 @@
-import{g as e}from"./main-vxz4gS_h.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
-//# sourceMappingURL=browser-D1UTzn5h.js.map
